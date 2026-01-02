7a9e146d73d25c865b39294e10c1769cf21072a2
Kubabob <j.m.m.bozek@gmail.com>
2026-01-02 19:11:39 +0100

    Implement SelectionMethod trait and tests

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,3 +1,6 @@
+use rand::seq::{IndexedRandom, SliceRandom};
+use rand::{Rng, RngCore};
+
 pub struct GeneticAlgorithm;
 
 impl GeneticAlgorithm {
@@ -23,12 +26,76 @@ pub trait Individual {
 }
 
 pub trait SelectionMethod {
-    fn select<'a, I>(&self, population: &'a [I]) -> &'a I
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
     where
         I: Individual;
+}
+
+pub struct RouletteWheelSelection;
+
+impl SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where
+        I: Individual,
+    {
+        population
+            .choose_weighted(rng, |individual| individual.fitness())
+            .expect("Got an empty population")
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::BTreeMap;
+    use std::iter::FromIterator;
+
+    #[derive(Debug, Clone)]
+    struct TestIndividual {
+        fitness: f32,
+    }
+
+    impl TestIndividual {
+        fn new(fitness: f32) -> Self {
+            Self { fitness }
+        }
+    }
+
+    impl Individual for TestIndividual {
+        fn fitness(&self) -> f32 {
+            self.fitness
+        }
+    }
+
+    mod selection_methods {
+        use rand::SeedableRng;
+        use rand_chacha::ChaCha8Rng;
+
+        use super::*;
+
+        #[test]
+        fn roulette_wheel_selection() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+            let population = vec![
+                TestIndividual::new(2.0),
+                TestIndividual::new(1.0),
+                TestIndividual::new(4.0),
+                TestIndividual::new(3.0),
+            ];
+
+            let mut actual_histogram = BTreeMap::new();
+            for _ in 0..50 {
+                let fitness = RouletteWheelSelection
+                    .select(&mut rng, &population)
+                    .fitness() as i32;
+
+                *actual_histogram.entry(fitness).or_insert(0) += 1;
+            }
+
+            let expected_histogram = BTreeMap::from_iter([(1, 4), (2, 10), (3, 14), (4, 22)]);
+
+            assert_eq!(actual_histogram, expected_histogram);
+        }
+    }
 }
