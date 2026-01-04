089400a34e16a83cabefc8b9d43cd7988c627594
Kubabob <j.m.m.bozek@gmail.com>
2026-01-04 14:11:38 +0100

    Now we have black triangles

--- www/index.js
+++ www/index.js
@@ -19,6 +19,22 @@ ctxt.scale(viewportScale, viewportScale);
 
 ctxt.fillStyle = "rgb(0,0,0)";
 
+CanvasRenderingContext2D.prototype.drawTriangle = function (x, y, size) {
+    this.beginPath();
+    this.moveTo(x, y);
+    this.lineTo(x + size, y + size);
+    this.lineTo(x - size, y + size);
+    this.lineTo(x, y);
+
+    this.fillStyle = "rgb(0,0,0)";
+    this.fill();
+};
+
 for (const animal of simulation.world().animals) {
-    ctxt.fillRect(animal.x * viewportWidth, animal.y * viewportHeight, 15, 15);
+    // ctxt.fillRect(animal.x * viewportWidth, animal.y * viewportHeight, 15, 15);
+    ctxt.drawTriangle(
+        animal.x * viewportWidth,
+        animal.y * viewportHeight,
+        0.01 * viewportWidth,
+    );
 }
