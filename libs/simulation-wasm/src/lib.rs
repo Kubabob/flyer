3029f840ad72cff11bbabad31d3d0a907831cead
Kubabob <j.m.m.bozek@gmail.com>
2026-01-04 14:06:50 +0100

    Black squares are visible! Hooray

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-use lib_simulation as sim;
+use lib_simulation::{self as sim};
 use rand::{prelude::*, rng};
 use wasm_bindgen::prelude::*;
 
@@ -18,5 +18,40 @@ impl Simulation {
         let sim = sim::Simulation::random(&mut rng);
 
         Self { rng, sim }
+    }
+
+    pub fn world(&self) -> World {
+        World::from(self.sim.world())
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug)]
+pub struct World {
+    #[wasm_bindgen(getter_with_clone)]
+    pub animals: Vec<Animal>,
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug)]
+pub struct Animal {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl From<&sim::World> for World {
+    fn from(world: &sim::World) -> Self {
+        let animals = world.animals().iter().map(Animal::from).collect();
+
+        Self { animals }
+    }
+}
+
+impl From<&sim::Animal> for Animal {
+    fn from(animal: &sim::Animal) -> Self {
+        Self {
+            x: animal.position().x,
+            y: animal.position().y,
+        }
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,24 @@
 import * as sim from "simulation-wasm";
 
 const simulation = new sim.Simulation();
+const viewport = document.getElementById("viewport");
+const viewportWidth = window.innerWidth;
+const viewportHeight = window.innerHeight;
+
+const viewportScale = window.devicePixelRatio || 1;
+
+viewport.width = viewportWidth * viewportScale;
+viewport.height = viewportHeight * viewportScale;
+
+viewport.style.width = viewportWidth + "px";
+viewport.style.height = viewportHeight + "px";
+
+const ctxt = viewport.getContext("2d");
+
+ctxt.scale(viewportScale, viewportScale);
+
+ctxt.fillStyle = "rgb(0,0,0)";
+
+for (const animal of simulation.world().animals) {
+    ctxt.fillRect(animal.x * viewportWidth, animal.y * viewportHeight, 15, 15);
+}
